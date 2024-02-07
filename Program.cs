2546bdede382493ce0fb0abe9a61f183f86d7933
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-07 21:06:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FilteringDataUsingQuery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //1. Data source
+            var numbers= new List<int>() {0, 1, 2, 3, 4, 5, 6};
+
+
+            
+
+            //Query execution
+            var evenNumbers = numbers.Where(num => num % 2 == 0).ToList();
+
+            numbers.Add(7);
+            numbers.Add(8);
+
+            foreach (int num in evenNumbers)
+            {
+                Console.Write("{0, 1} ", num);
+            }
+
+            //static bool FilterEvenNumber(int num)
+            //{
+
+            //    return num % 2 == 0;
+            //}
+        }
+    }
+}
